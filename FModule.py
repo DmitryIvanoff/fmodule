d051bdf98f12f6a3b22f393af50c936ce1339fea
DmitryIvanoff <divan.9608@gmail.com>
2019-02-04 21:52:24 +0300

    added predictions

--- FModule.py
+++ FModule.py
@@ -24,7 +24,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from bitarray import bitarray
 from zipfile import ZipFile
-
+import random as rand
 
 def ema(series, alpha, ma=None):
     """
@@ -39,6 +39,24 @@ def ema(series, alpha, ma=None):
         ma = series[0]
     for f in series:
         yield alpha*f+(1.-alpha)*ma
+
+
+def open_hook(filename, mode):
+    '''
+
+    :param filename:
+    :param mode:
+    :return:
+    '''
+    if filename.endswith(('.csv', '.txt')):
+        return open(filename, mode)
+    if filename.endswith('.zip'):
+        try:
+            zf = ZipFile(filename)
+            return zf.open(filename.replace('.zip', '.csv'),mode=mode)
+        except Exception as e:
+            raise e
+
 
 
 def fileStream(files, batch_size=32, *args, **kwargs):
@@ -80,17 +98,17 @@ def formBinarySeries(stream, *args, **kwargs):
     nod = kwargs.get('nod', 2)  # число производных (f'(t),f''(t) as default)
 
     # init for ma series
-    series = [[]]                         # series = [(ma>f(t),f'(t)>0,f''(t)>0,...] , len(series)=nod+1
+    series = [bitarray()]                         # series = [(ma>f(t),f'(t)>0,f''(t)>0,...] , len(series)=nod+1
     t_1 = [None]                          # previous values [ma(t-1),f(t-1),v(t-1)]
     ma = None
 
     # init for derivatives series
     for i in range(nod):
         t_1.append(0.0)
-        series.append([])
+        series.append(bitarray())
     for batch in stream:
         for i in range(nod+1):
-            series[i] = []
+            series[i] = bitarray()
         for (ma, f) in zip(ema(batch, alpha=alpha, ma=t_1[0]), batch):
             series[0].append(ma > f)
             fi_dt = f
@@ -100,7 +118,7 @@ def formBinarySeries(stream, *args, **kwargs):
                 t_1[i] = fi_dt
                 fi_dt = dfi_dt
         t_1[0] = ma
-        yield (np.array(bin_s, dtype=bool) for bin_s in series)
+        yield (bin_s for bin_s in series)
 
 
 def predict( stream, *args, **kwargs):
@@ -124,74 +142,97 @@ def predict( stream, *args, **kwargs):
         hists.append(np.zeros((2 << bufflength-1, max_offset), dtype=dt))
         offsets.append(np.zeros(2 << bufflength-1, dtype=dt))
         slbuffers.append(init_buffer)
-    count = 0
+    time = 0
+    stakes = np.zeros(3)
     print('Поехали!')
     # ------------
 
     for binseries_batches in formBinarySeries(stream, {'nod': nod}):
-        for series in zip(binseries_batches):
-            count += 1
-            if not (count%100000):
-                print('Живем!!', np.sum(hists[0]))
+        for series in zip(*binseries_batches):
+            time += 1
+            if not time % 100000:
+                print('Живем!!', np.sum(hists[0]), stakes / time)
             for i in range(len(series)):
+                # move buffer
                 slbuffers[i].pop(0)
+                buff = bitarray(slbuffers[i])
                 slbuffers[i].append(series[i])
-                val_index = int.from_bytes(slbuffers[i].tobytes(), 'big')
+
+                # calc probability of 1
+                buff.append(True)
+                # get buff as int
+                val_index = int.from_bytes(buff.tobytes(), 'big')
+                # get offset
                 m, time_index = divmod(offsets[i][val_index], max_offset)
                 if not m:
+                    probability_1 = hists[i][val_index, time_index]
+                else:
+                    probability_1 = 0
+                buff.pop()
+
+                # calc probability of 0
+                buff.append(False)
+                val_index = int.from_bytes(buff.tobytes(), 'big')
+                m, time_index = divmod(offsets[i][val_index], max_offset)
+                if not m:
+                    probability_0 = hists[i][val_index, time_index]
+                else:
+                    probability_0 = 0
+
+                # make prediction
+                if probability_1 != probability_0:
+                    prediction = (probability_1 > probability_0)
+                else:
+                    prediction = rand.randint(0,1)
+                # calc accuracy
+                if not (prediction ^ series[i]):
+                    stakes[i] += 1
+
+                val_index = int.from_bytes(slbuffers[i].tobytes(), 'big')
+                m, time_index = divmod(offsets[i][val_index], max_offset)
+
+                # update histogram
+                if not m:
                     hists[i][val_index, time_index] += 1
+
+                # update offsets
                 offsets[i][val_index] = 0
                 offsets[i] += 1
     return hists
 
-def drawHist(hist,title):
+
+def drawHist(hist, ax,title, save=False, *args, **kwargs):
     '''
 
     :param hist:
     :return:
     '''
-    fig,(ax1) = plt.subplots(1,1,figsize=(1, 1))
-    fig.set_size_inches(20,20)
-    cmap = plt.cm.afmhot_r
-    #hist =(hist)/np.sum(hist))
-    #norm = cm.colors.Normalize(vmax=np.sum(hist), vmin=0)
-    print(hist)
-    print(np.amax(hist))
-    im = ax1.imshow(hist,#interpolation='bilinear',
+
+    cmap = kwargs.get('cmap', plt.cm.afmhot_r)
+    norm = kwargs.get('norm', mpl.colors.LogNorm(vmin=0.0000001, vmax=np.sum(hist)))
+    # hist =(hist)/np.sum(hist))
+    # norm = cm.colors.Normalize(vmax=np.sum(hist), vmin=0)
+    # print(hist)
+    # print(np.amax(hist))
+    im = ax.imshow(hist,  # interpolation='bilinear',
                     cmap=cmap,
                     origin='lower',
                     extent=[0, 256, 0,256],
-                    norm=mpl.colors.LogNorm(vmin=0.0000001, vmax=np.sum(hist)),
+                    norm=norm,
                     aspect='auto')
-    fig.colorbar(im,cmap=cmap)
-    ax1.set(xlabel='time', ylabel='value of buffer',title=title)
-    fig.savefig('{}.png'.format(title),dpi=100,format='png')
-    plt.show()
-
-
-def open_hook(filename, mode):
-    '''
-
-    :param filename:
-    :param mode:
-    :return:
-    '''
-    if filename.endswith(('.csv', '.txt')):
-        return open(filename, mode)
-    if filename.endswith('.zip'):
-        try:
-            zf = ZipFile(filename)
-            return zf.open(filename.replace('.zip', '.csv'),mode=mode)
-        except Exception as e:
-            raise e
+    ax.set(xlabel='time', ylabel='value of buffer', title=title)
+    if save:
+        plt.imsave('{}.png'.format(title), hist, dpi=100, format='png')
+    return im
 
 
 if __name__ == '__main__':
 
     files = ['data/points_s.csv']#['C25600000.zip']
     hists = predict(fileStream(files))
-    drawHist(hists[0], 'Histogram of Binary ma(t)')
-    drawHist(hists[1], "Histogram of Binary f'(t)")
-    drawHist(hists[2], "Histogram of binary f''(t)")
-
-
+    nod = 2
+    fig, axes = plt.subplots(nod+1,1,figsize=(1,1))
+    fig.set_size_inches(3*(nod+1),20)
+    for i in range(nod+1):
+        image = drawHist(hists[i], axes[i], "Histogram of Binary f'{}".format(i), save=True)
+        plt.colorbar(image, ax=axes[i])
